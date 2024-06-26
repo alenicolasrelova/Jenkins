eaf36b69f0b58543df13b2b9fbcc1c5bba40f052
Alejandro Relova <alenicolasrelova@gmail.com>
2024-06-26 17:37:12 +0000

    Modificando Workspace para el sock de docker

--- jenkins/test/mvn.sh
+++ jenkins/test/mvn.sh
@@ -3,6 +3,6 @@
 echo "***************************"
 echo "** Testing the code ***********"
 echo "***************************"
-WORKSPACE=/srv/jenkins-docker/pipeline/
+WORKSPACE=/srv/jenkins-proyect/workspace/Final_Project/java-app
 
 docker run --rm  -v  $WORKSPACE/java-app:/app -v /root/.m2/:/root/.m2/ -w /app maven:3-alpine "$@"
